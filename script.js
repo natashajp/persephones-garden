28e881c8d2ede06e1387ee57e860b5dbc8c851e8
natashajp <njperson1219@gmail.com>
2023-07-08 03:10:26 -0400

    Create proactive farming actions (tilling the land and growing flowers)

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
-//Create garden
+//Cache individual garden tiles (Lord help me)
 
-const tile0 = document.getElementsByClassName('zero');
+const indvTile0 = document.getElementById('zero0')
+const indvTile1 = document.getElementById('zero1')
+const indvTile2 = document.getElementById('zero2')
+const indvTile3 = document.getElementById('zero3')
+const indvTile4 = document.getElementById('zero4')
+const indvTile5 = document.getElementById('zero5')
 
-for (let i = 0; i < tile0.length; i++) {
-    const grass0 = document.createElement('img');
-    grass0.src = 'assets/grass0.png';
-    grass0.setAttribute('width', '64');
-    grass0.setAttribute('height', '64');
-    tile0[i].appendChild(grass0);
-}
+const indvTile6 = document.getElementById('zero6')
+const indvTile7 = document.getElementById('zero7')
+const indvTile8 = document.getElementById('zero8')
+const indvTile9 = document.getElementById('zero9')
+const indvTile10 = document.getElementById('zeroA')
+const indvTile11 = document.getElementById('zeroB')
 
-const tile1 = document.getElementsByClassName('one');
+const indvTile12 = document.getElementById('zeroD')
+const indvTile13 = document.getElementById('zeroE')
+const indvTile14 = document.getElementById('zeroF')
+const indvTile15 = document.getElementById('oneZero')
+const indvTile16 = document.getElementById('oneOne')
+const indvTile17 = document.getElementById('oneTwo')
 
-for (let i = 0; i < tile1.length; i++) {
-    const grass1 = document.createElement('img');
-    grass1.src = 'assets/grass1.png';
-    grass1.setAttribute('width', '64');
-    grass1.setAttribute('height', '64');
-    tile1[i].appendChild(grass1);
-}
+const indvTile18 = document.getElementById('oneThree')
+const indvTile19 = document.getElementById('oneFour')
+const indvTile20 = document.getElementById('oneFive')
+const indvTile21 = document.getElementById('oneSix')
+const indvTile22 = document.getElementById('oneSeven')
+const indvTile23 = document.getElementById('oneEight')
 
-const tile2 = document.getElementsByClassName('two');
+const indvTile24 = document.getElementById('oneNine')
+const indvTile25 = document.getElementById('oneA')
+const indvTile26 = document.getElementById('oneB')
+const indvTile27 = document.getElementById('oneC')
+const indvTile28 = document.getElementById('oneD')
+const indvTile29 = document.getElementById('oneE')
 
-for (let i = 0; i < tile2.length; i++) {
-    const grass2 = document.createElement('img');
-    grass2.src = 'assets/grass2.png';
-    grass2.setAttribute('width', '64');
-    grass2.setAttribute('height', '64');
-    
-    tile2[i].appendChild(grass2);
-}
+const indvTile30 = document.getElementById('oneF')
+const indvTile31 = document.getElementById('twoZero')
+const indvTile32 = document.getElementById('twoOne')
+const indvTile33 = document.getElementById('twoTwo')
+const indvTile34 = document.getElementById('twoThree')
+const indvTile35 = document.getElementById('twoFour')
 
-const tile3 = document.getElementsByClassName('three');
-
-for (let i = 0; i < tile3.length; i++) {
-    const grass3 = document.createElement('img');
-    grass3.src = 'assets/grass3.png';
-    grass3.setAttribute('width', '64');
-    grass3.setAttribute('height', '64');
-    tile3[i].appendChild(grass3);
-}
 
 //Create inventory
 
@@ -83,58 +85,514 @@ sickleIcon.setAttribute('width', '48');
 sickleIcon.setAttribute('height', '48');
 sickle.appendChild(sickleIcon);
 
-//Create intermediary soil tiles
-
-const soilIcon = document.createElement('img');
-soilIcon.src = 'assets/soil.png';
-soilIcon.setAttribute('width', '64');
-soilIcon.setAttribute('height', '64');
-
-const sownSeeds = document.createElement('img');
-sownSeeds.src = 'assets/soil.png';
-sownSeeds.setAttribute('width', '64');
-sownSeeds.setAttribute('height', '64');
-
-const wetSoilIcon = document.createElement('img');
-wetSoilIcon.src = 'assets/soil.png';
-wetSoilIcon.setAttribute('width', '64');
-wetSoilIcon.setAttribute('height', '64');
-
-//Create flower tiles
-
-const blueFlower = document.createElement('img');
-blueFlower.src = 'assets/blue_flower.png';
-blueFlower.setAttribute('width', '64');
-blueFlower.setAttribute('height', '64');
-blueFlower.className = "zero";
-
-const pinkFlower = document.createElement('img');
-pinkFlower.src = 'assets/pink_flower.png';
-pinkFlower.setAttribute('width', '64');
-pinkFlower.setAttribute('height', '64');
-pinkFlower.className = "one";
-
-const purpleFlower = document.createElement('img');
-purpleFlower.src = 'assets/purple_flower.png';
-purpleFlower.setAttribute('width', '64');
-purpleFlower.setAttribute('height', '64');
-purpleFlower.className = "two";
-
-const yellowFlower = document.createElement('img');
-yellowFlower.src = 'assets/yellow_flower.png';
-yellowFlower.setAttribute('width', '64');
-yellowFlower.setAttribute('height', '64');
-yellowFlower.className = "three";
 
 //Create farming actions
 
-//Create universal grass DOM element
 
-const grassTile = document.querySelectorAll(".grass");
+//Declare toolMode variable
 
-//Hoeing the land
+let toolMode;
 
-// hoe.addEventListener('click' , function() {
-//     hoe.replaceChild(yellowFlower, hoe.childNodes[0])
-// });
 
+//Apply toolMode variable to inventory
+
+hoe.addEventListener('click' , function() {
+    toolMode = 0;
+    // console.log("Tool mode is set to 'Hoe' (0).")
+    console.log(toolMode);
+});
+
+seeds.addEventListener('click' , function() {
+    toolMode = 1;
+    // console.log("Tool mode is set to 'Hoe' (0).")
+    console.log(toolMode);
+});
+
+can.addEventListener('click' , function() {
+    toolMode = 2;
+    // console.log("Tool mode is set to 'Hoe' (0).")
+    console.log(toolMode);
+});
+
+pesticide.addEventListener('click' , function() {
+    toolMode = 3;
+    // console.log("Tool mode is set to 'Hoe' (0).")
+    console.log(toolMode);
+});
+
+sickle.addEventListener('click' , function() {
+    toolMode = 4;
+    // console.log("Tool mode is set to 'Hoe' (0).")
+    console.log(toolMode);
+});
+
+
+//Add event listeners to grass tiles (Lord help me, again)
+
+indvTile0.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile0.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile0.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile0.src = 'assets/pink_flower.png'
+    }
+})
+
+indvTile1.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile1.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile1.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile1.src = 'assets/yellow_flower.png'
+    }
+})
+
+indvTile2.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile2.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile2.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile2.src = 'assets/blue_flower.png'
+    }
+})
+
+indvTile3.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile3.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile3.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile3.src = 'assets/pink_flower.png'
+    }
+})
+
+indvTile4.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile4.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile4.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile4.src = 'assets/purple_flower.png'
+    }
+})
+
+indvTile5.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile5.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile5.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile5.src = 'assets/pink_flower.png'
+    }
+})
+
+indvTile6.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile6.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile6.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile6.src = 'assets/purple_flower.png'
+    }
+})
+
+indvTile7.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile7.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile7.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile7.src = 'assets/yellow_flower.png'
+    }
+})
+
+indvTile8.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile8.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile8.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile8.src = 'assets/yellow_flower.png'
+    }
+})
+
+indvTile9.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile9.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile9.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile9.src = 'assets/purple_flower.png'
+    }
+})
+
+indvTile10.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile10.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile10.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile10.src = 'assets/purple_flower.png'
+    }
+})
+
+indvTile11.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile11.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile11.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile11.src = 'assets/blue_flower.png'
+    }
+})
+
+indvTile12.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile12.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile12.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile12.src = 'assets/blue_flower.png'
+    }
+})
+
+indvTile13.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile13.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile13.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile13.src = 'assets/blue_flower.png'
+    }
+})
+
+indvTile14.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile14.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile14.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile14.src = 'assets/blue_flower.png'
+    }
+})
+
+indvTile15.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile15.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile15.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile15.src = 'assets/pink_flower.png'
+    }
+})
+
+indvTile16.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile16.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile16.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile16.src = 'assets/yellow_flower.png'
+    }
+})
+
+indvTile17.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile17.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile17.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile17.src = 'assets/pink_flower.png'
+    }
+})
+
+indvTile18.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile18.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile18.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile18.src = 'assets/pink_flower.png'
+    }
+})
+
+indvTile19.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile19.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile19.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile19.src = 'assets/pink_flower.png'
+    }
+})
+
+indvTile20.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile20.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile20.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile20.src = 'assets/blue_flower.png'
+    }
+})
+
+indvTile21.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile21.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile21.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile21.src = 'assets/blue_flower.png'
+    }
+})
+
+indvTile22.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile22.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile22.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile22.src = 'assets/purple_flower.png'
+    }
+})
+
+indvTile23.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile23.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile23.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile23.src = 'assets/blue_flower.png'
+    }
+})
+
+indvTile24.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile24.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile24.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile24.src = 'assets/purple_flower.png'
+    }
+})
+
+indvTile25.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile25.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile25.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile25.src = 'assets/pink_flower.png'
+    }
+})
+
+indvTile26.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile26.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile26.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile26.src = 'assets/yellow_flower.png'
+    }
+})
+
+indvTile27.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile27.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile27.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile27.src = 'assets/blue_flower.png'
+    }
+})
+
+indvTile28.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile28.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile28.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile28.src = 'assets/yellow_flower.png'
+    }
+})
+
+indvTile29.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile29.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile29.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile29.src = 'assets/blue_flower.png'
+    }
+})
+
+indvTile30.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile30.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile30.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile30.src = 'assets/blue_flower.png'
+    }
+})
+
+indvTile31.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile31.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile31.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile31.src = 'assets/purple_flower.png'
+    }
+})
+
+indvTile32.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile32.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile32.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile32.src = 'assets/yellow_flower.png'
+    }
+})
+
+indvTile33.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile33.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile33.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile33.src = 'assets/pink_flower.png'
+    }
+})
+
+indvTile34.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile34.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile34.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile34.src = 'assets/yellow_flower.png'
+    }
+})
+
+indvTile35.addEventListener('click', function() {
+    switch (toolMode) {
+        case 0:
+            indvTile35.src = 'assets/soil.png';
+            break;
+        case 1:
+            indvTile35.src = 'assets/sown_seeds.png';
+            break;
+        case 2:
+            indvTile35.src = 'assets/pink_flower.png'
+    }
+})
